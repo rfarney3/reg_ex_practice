9e7462e3a8df18e3092792d8025b63b5c6ce5f5e
rfarney3 <farneyr@yahoo.com>
2018-06-24 17:28:18 -0700

    using exec

--- reg_ex.js
+++ reg_ex.js
@@ -1,3 +1,11 @@
+//REPLACE
+//replacing one character
 console.log("Hello World".replace(/H/g,"B")) //"Bello World"
-
+//replacing multiple characters
 console.log("Hello World".replace(/H|W/g,"B")) //"Bello Borld"
+
+//EXEC => MATCHING
+console.log(/ab*/g.exec("Hella World")) // ["a", index: 4, input: "Hella World"]
+
+let matches = /(hello \S+)/.exec("This is a hello world!")
+console.log(matches[1]); // hello world!
